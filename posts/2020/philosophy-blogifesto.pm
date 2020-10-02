438286814d66896ceeab6cda5f629fbce702ffda
Mike Deigan <michael.deigan@yale.edu>
2020-10-02 15:43:55 -0400

    Added 1st post

---
+++ posts/2020/philosophy-blogifesto.pm
@@ -0,0 +1,160 @@
+#lang pollen
+
+◊(define-meta title "A Philosophy Blogifesto")
+◊(define-meta date "2020-10-02")
+◊(define-meta month-day "10-02")
+◊(define-meta uuid "8e94180b-fff7-4c7b-b499-d1b76e46220c")
+◊(define-meta feed-date "2020-10-02T14:44:54-04:00")
+◊(define-meta topics "philosophy online")
+◊(define-meta summary "The internet needs more philosophy blogs!")
+
+
+
+◊lead-in{More philosophy online, please!}
+The web would be better with more philosophy on it and
+philosophy would be better with more of it on the web. 
+
+
+Sure, the journals are already online, even if most
+are ludicrously
+paywalled.◊note{Props to the Open Access exceptions:
+◊link["https://www.philosophersimprint.org/"]{Phil Imprint}, 
+◊link["https://www.ergophiljournal.org/"]{Ergo},
+◊link["http://jesp.org/index.php/jesp/index"]{JESP},
+◊link["http://www.jpe.ox.ac.uk/"]{JPE},
+◊link["https://jmphil.org/"]{JMPhil},
+◊link["https://semprag.org/index.php/sp"]{S&P}, and
+◊link["https://philpapers.org/journals/pubs.pl?listId=14954&journals=1"]{others}!}
+	A lot of books are available online too, 
+		one way or another.
+And of course there are the wonderful EPs: the
+◊link["https://plato.stanford.edu/"]{SEP} and
+◊link["https://iep.utm.edu/"]{IEP}.
+Reading all the great philosophy already online
+would take more time than anyone has. 
+
+But it can take years to get an idea out to readers in these
+professionally vetted forms, and to do so one needs to package
+it in just the right way to satisfy---in the eyes
+of reviewers and editors---a variety of
+constraints:  concision, clarity,
+rigor, scholarliness,
+originality, 'relevance', and
+ word limits.
+
+Besides, many ideas that
+people should  be able to read and discuss online
+simply are not publishable in these places: 
+the half-baked, the politically opinionated,
+the minor nit-pick, the non-critical notice,
+the unargued synoptic vision, the reading list,
+the cool paragraph you just came across in some
+obscure place, etc.
+
+Finally, sometimes one just doesn't feel like reading stuff
+that has passed through this wringer, you know?
+Scholarliness, rigor, and the rest is
+ important for certain purposes,
+but sometimes one just feels like getting
+some philosophy that's rougher, punchier,  sillier, smaller,
+grander, or
+more personal than what typically gets into the academic
+journals.◊note{I'm not being original in 
+ complaining about academic publishing in these
+ ways. See Agnes Callard's 
+◊link["https://thepointmag.com/examined-life/publish-and-perish-agnes-callard/"]{recent article in ◊i{The Point}} for one recent
+entry in the genre.
+I'm sympathetic to some of what she
+says there, but also think she underestimates the ambition,
+	quality, and depth of recent work relative 
+	to the older stuff.}
+
+
+I think this is part of what's attractive about reading
+the great (and even the not so great) philosophers of the past: they
+had their own constraints, but not the same
+ones we have. Lack of the constraints is also
+part of what's great about talking philosophy with friends
+in a pub, department lounge, or wherever.
+But opportunities for that are limited,
+especially these days.
+ 
+
+Luckily, there are many other possibilities for doing
+philosophy online.
+Blogs and microblogs; vlogs, video essays, and streams;
+podcasts and microcasts;
+newsletters, wikis, interactive tutorials, forums, webcomics,
+	webzines, and online books. You can put online your
+reading notes, lecture notes, teaching ideas, 
+article ideas, ideas too small for articles, ideas
+too big for articles,
+book reviews,
+lit reviews, interviews, and link dumps. 
+Scholarly or slap-dash, many times revised
+or first pass outlines, any style under the sun. 
+Public philosophy, deep-in-the-arcane-weeds philosophy,
+and everything in between. 
+
+There's a lot of philosophy online in most of these forms too, but I wish
+there was more of it, from a wider range of philosophers.
+
+
+
+◊p[#:style "text-align: right"]{
+◊emph{"Why not contribute some yourself?"}}
+
+Okay, sure, why not?
+
+
+◊section{}
+
+This is my new philosophy blog. 
+
+I'd be happy if you too would start a philosophy blog (or whatever 
+other kind of online philosophy thing).
+Tell me about it so I can check it out and maybe talk about it here.
+One thing I hope to regularly do here is talk about what's
+going on philosophy-wise elsewhere online. 
+
+
+◊section{}
+◊p[#:style "text-align: right;"]{
+◊emph{"
+Why not just post on Twitter or Facebook
+or Medium or ...?"}} 
+
+As this is a blogifesto, it is fitting to have
+some high-minded numbered
+principles to which I solemnly commit myself.
+
+They
+are:
+
+◊ol[#:style "font-variant: small-caps"]{
+◊li{◊emph{No distracting ads or nagware.}}
+◊li{◊emph{No invasive surveillance.}}
+◊li{◊emph{No volunteering control of my content 
+to some corporation for use in algorithms that manipulate
+people into addictive scrolling and clicking and posting  in order
+to generate more revenue from distracting ads targeted with invasive
+surveillance.}}
+}
+
+There's a lot of fun and interesting philosophy talk
+on Twitter (and, I assume, on Facebook), 
+but it seems to me that on balance these platforms make the internet and
+the world worse. Given their profit models, I don't expect
+them to improve. So we should work towards better alternatives. 
+Blogging from my own website is my small contribution.
+◊end{}
+
+◊;Twitter is bad, Facebook is worse. They are free only as
+◊;zoos are free for zebras. The real customers---advertisers---pay
+◊;plenty.
+
+◊;There should be more self-published
+◊;philosophy on the web, but ideally without distracting ads and
+◊;surveillance.
+
+
