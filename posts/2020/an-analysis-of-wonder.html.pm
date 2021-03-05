8700e18c4dc057e0c59b4dfa0a21815f66302756
Mike Deigan <michael.deigan@yale.edu>
2021-03-05 18:20:49 -0500

    Added analysis of wonder post (from October 2020)

---
+++ posts/2020/an-analysis-of-wonder.html.pm
@@ -0,0 +1,430 @@
+#lang pollen
+
+◊(define-meta title "An Analysis of Wonder")
+◊(define-meta date "2020-10-19")
+◊(define-meta month-day "10-19")
+◊(define-meta feed-date "2020-10-19T10:47:44-04:00")
+◊(define-meta topics "philosophy; epistemology")
+◊(define-meta uuid "f04f2d57-9dae-47b9-94b2-7474f87804f9")
+
+◊(define-meta summary "Wondering is wanting to know")
+
+
+◊lead-in{What is it to wonder?}
+ I'm going to defend an analysis 
+that I think might be right:
+to wonder is to want to know.
+This should be of interest especially to epistemologists,
+given wondering's central role in inquiry.◊note{There seems
+				to be something of
+				an inquisitive turn going on
+				in epistemology, continuing a rich
+				tradition that goes back to Plato
+				and Aristotle.  See, e.g., recent
+				papers by Jane Friedman 
+((2013) "◊link["https://doi.org/10.1111/phpe.12026" #:ext 1]{Question-directed attitudes}",
+(2017) "◊link["https://doi.org/10.1111/nous.12137" #:ext 1]{Why Suspend Judging?},
+(2019) "◊link["https://doi.org/10.1111/nous.12222" #:ext 1]{Inquiry and Belief}",
+(forthcoming) "◊link["https://jfriedmanphilo.github.io/E&Z.pdf" #:ext 1]{The Epistemic and the Zetetic}"),
+				and Christoph Kelp
+((2014)	"◊link["https://www.jstor.org/stable/24475391" #:ext 1]{Two for the Knowledge Goal of Inquiry}"
+(2019) "◊link["https://doi.org/10.1111/phpr.12489" #:ext 1]{Inquiry and the Transmission of Knowledge}", 
+(2020) "◊link["https://doi.org/10.1111/phpr.12719" #:ext 1]{Theory of inquiry}").}
+◊section{Why Analyze?}
+
+Before getting into the analysis,  a quick
+methodological preamble.
+I agree with those who think epistemologists shouldn't
+get too hung up on finding analyses of the various states and
+relations they are interested in: knowledge, belief, 
+evidence, etc.◊note{E.g.,
+Williamson (2000) ◊i{Knowledge and Its Limits}  and 
+Pasnau (2017) ◊i{After Certainty}.} What we really want are good
+descriptive and normative theories of these things, or
+whatever things in the ballpark turn out to be most
+interesting or important. And we shouldn't presume 
+that the good theories will make significant use of
+reductive analyses. Fortunately, most epistemology going
+on these days doesn't presume this, and doesn't focus
+too much on finding analyses.
+
+But this isn't to say that if there ◊emph{is} some
+true reductive analysis involving important epistemic notions
+that it isn't worth knowing.
+Analyses make for nice theoretical
+connective tissue, letting you port over independently motivated
+views
+about the
+analyzing notions to the analyzed one and vice versa. They also at
+least give you the option of claiming a kind of parsimony:
+Defined Thing, you might say, just is Defining Thing, so 
+we no longer need to treat these as separate ontological 
+commitments.
+Or if you prefer lush to arid landscapes, you
+have the option of saying that we have given a real definition,
+revealing something's essence. Analyses aren't the end-all, 
+be-all, nor are they necessary preliminaries to theorizing,
+but they're good if you
+can get it's ’em.
+
+
+◊;It is in this spirit I offer the analysis of wonder.
+
+◊section{The Analysis}
+Okay, so here it is:
+◊blockquote{◊header{◊b{WTK Theory of Wonder}}
+	◊div[#:style"text-align:center"]{S wonders Q iff S wants to know Q.
+	}}
+
+For instance, I want to know who will win the election, 
+so it follows that I wonder who will win the election.
+I wonder how many degrees of warming there will be 
+this century, so I must want to know how many degrees
+of warming
+there will be this century.
+It's not the case that I want to know exactly how many blades of grass
+there are in the nearby park, so that's not something I wonder.
+And I don't wonder
+whether it's raining in Topeka, so it must be that
+it's not the case that I want to know whether it's raining in Topeka.
+
+Results so plausible as to seem trivial. Perhaps
+this is why no philosophers have defended this analysis.◊note{As
+far as I know. Please tell me if I'm missing some
+literature on this. Note also that some
+linguists---◊link["https://doi.org/10.1007/BF00351935" #:ext 1]{Kartunnen (1977)},
+◊link["https://doi.org/10.1007/s10988-007-9014-x" #:ext 1]{Guerzoni and Sharvit (2007)}---have
+suggested a semantics for wonder along these lines. ◊link["https://doi.org/10.1093/jos/ffv009" #:ext 1]{Uegaki (2016)}
+argues that on this analysis, plus some assumptions about
+◊i{want}’s presuppositions and their projection,
+one can explain why we can't say that someone wonders ◊i{that p}.
+This is a cool result, but 
+see ◊link["https://doi.org/10.1007/s11050-019-09152-9" #:ext 1]{Theiler,
+Roelofsen, & Aloni (2019, §A.2)}
+for reasons to think
+this won't generalize to other clause-embedding predicates
+that don't take declaratives. They also raise a problem to do
+with presupposition
+projection which I might address at some later date.}
+But in fact the analysis is inadequate as it stands.
+We need to get clearer on what kind of wanting is involved,
+which will help us avoid some apparent problems for the
+analysis. We also need to deal with a problem about metacognition.
+
+◊section{What-things-considered?}
+
+Here's a class of potential counterexamples: sometimes
+we find ourselves wondering something when we don't
+◊i{really} want to know. Suppose you're wondering 
+how many years you have left to live. But you also think
+that if you could somehow know the answer to this, it
+would cast such a pall over the rest of your life
+that any benefits to knowing would be outweighed. If
+someone offered you a glimpse into the crystal ball,
+you would turn it down, because you
+don't want to know. Nevertheless, you could still
+be wondering how many years you have left to live.
+So it looks like we have wondering without wanting to
+know.
+Another analysis bites the dust, it seems.
+
+Not so fast! ◊i{Distinguo:} all-things-considered wanting
+	and some-things-considered wanting. 
+
+Suppose 
+you invite me to play tennis on Tuesday at 2:00.
+I would really enjoy this, need the exercise,
+	and would be happy to have an excuse
+to catch up, but I'm scheduled to teach at that time.
+As Wayne Davis points out,◊note{Davis (1984) 
+"◊link["https://www.jstor.org/stable/4319662" #:ext 1]{The Two
+Senses of Desire}": p. 239.} I can truly describe my mental
+state in a pair of contrasting ways:
+◊ul{
+◊li{"I want to play, but I have to teach."}
+◊li{"I don't want to play, since I have teach."}
+}
+
+The former reports that I ◊i{some-things-considered} want
+to play, whereas the latter reports that I ◊i{all-things-considered}
+want not to play.
+
+This distinction allows us to avoid the counterexample.
+Wondering, we should say, is wanting to know some-things-considered.
+When you wonder when you will die, you want to know some-things-considered,
+even if all-things-considered you'd rather not know.
+That seems right to 
+me.
+
+
+◊section{Activity}
+
+Here's another concern about the WTK Theory.◊note{Friedman (2013, p. 154) 
+objects along these lines. Thanks also to Zoltán Szabó for raising it
+in conversation.} The verb ◊i{to wonder}
+seems to express an activity, whereas ◊i{to want to know}
+expresses a state. They have different aspectual classes.◊note{For a classic division of verbs
+(or really, verb phrases) by aspectual class,
+see Zeno Vendler (1967) ◊i{Linguistics in
+Philosophy}: Ch. 4.}
+
+One mark distinguishing these classes of verbs is
+that statives don't take well to the progressive,
+whereas non-statives do. 
+Picture a scene:
+your friend is sitting at their desk, pulling
+the Thinker pose.  Contrast the following as descriptions
+of what's going on.
+◊ul{
+◊li{"They're thinking that expressivism might be true."}
+◊li{ "They're believing that expressivism might be true."}
+}
+The latter sounds terrible! 
+This is because ◊i{believe} is a stative verb, in contrast
+with ◊i{think}, which is non-stative. Belief is a state you can
+be in, whereas thinking is some kind of activity---something
+that happens.
+
+Now let's try this test for ◊i{wonder}
+	and ◊i{want}.
+◊ul{
+◊li{"They're wondering whether expressivism can make sense of normative
+inquiry."}
+◊li{"They're wanting to know whether expressivism can make sense of
+normative inquiry."}
+}
+Again, the latter sounds terrible! It seems that ◊i{want} is a
+stative verb and ◊i{wonder} is not. Desire is a state you can
+be in, whereas wondering is some kind of activity---something
+that happens.
+
+This looks bad for the WTK Theory of Wonder.
+A successful analysis of wonder will have to identify wonder
+not with a state like desire, but with something non-stative, some
+kind of activity
+or happening.
+
+Here's one alternative proposal: trying to find out. This gets many
+normal cases right. When I'm wondering what I should cook
+for dinner, I am trying to find out what I should cook for 
+dinner by thinking through what ingredients I have, how 
+long various options would take and how much effort they
+would involve. And it's not easy to think of cases where
+one is trying to find something out but doesn't wonder the
+relevant question.
+
+That said, I don't think it gets all the
+cases right. It has trouble, first of all,
+with the merely some-things-considered
+wanting to know cases. You can be wondering when you
+will die while purposefully ◊i{not} trying to find
+out the answer to that.
+Second, it has trouble with cases of wondering what you
+realize you'll never know. I sometimes wonder what Mozart
+would have composed had he not died young, but I've never
+tried to find out, since it's so transparently hopeless.
+
+
+I think a better way to go here preserves the WTK theory, again by
+making a distinction between types of wanting. Distinguish, as
+Goldman does,◊note{Goldman attributes the distinction to Alston, but no
+doubt it goes back further, in some form or other.}
+between ◊i{occurrent} and ◊i{non-occurrent} 
+wants. Here's how he introduces the distinction:
+◊blockquote{
+John is concentrating on finishing the lawn by six o'clock.
+He is giving all his attention to mowing it as quickly as 
+possible, with the thought of getting it done by six. During this
+period, whether it be a few seconds or a whole minute, John has
+an ◊i{occurrent} want to finish mowing the lawn by six o'clock.
+The thought of finishing the lawn by six ◊i{occurs} to hum, occupies
+his attention, fills his consciousness. It is a datable event or 
+process. During the same period it would also be correct to say
+that John wanted to be president of his company. But his wanting
+to be president of the company is not an ◊i{occurrent} want during
+this time, for John has not been thinking about the company or his
+future at the company. Nevertheless, we could say that John wants
+to be president of his company because he has a ◊i{disposition} to
+have occurrent wants to this effect, and this disposition is present while
+John is mowing the lawn, though it is not being manifested. I shall
+say that John has a ◊i{standing} want to be president of his company.
+◊footer{Alvin Goldman (1970, p. 86) ◊br{} ◊i{A Theory of Human Action}}
+}
+Pinning down what precisely this distinction amounts to is no
+easy task,◊note{See Bartlett (2018) "◊link["https://doi.org/10.1080/00455091.2017.1323531" #:ext 1]{Occurrent States}" 
+for one recent attempt.}
+but I take it to be clear enough as is for my purposes.
+Note that this kind of want is not just a philosopher's postulate.
+We do have words for at least some such wanting events. In
+English:
+longings, cravings, hankerings, lustings, yearnings.
+It would be a bit over the top to say that
+John was yearning to finish mowing the lawn, but it is something
+that could be said if he wanted it badly enough (and perhaps
+met some other conditions required for yearning).
+
+What's important is that there is a kind of wanting that is
+a "datable event or process", a non-stative kind of thing. We
+can naturally use the progressive here.
+John is occurrently wanting to finish mowing the lawn by six,
+craving chocolate cake, etc.
+
+We can solve the non-stative problem for the WTK Theory,
+then, by saying that the kind of wanting it identifies 
+wondering with is occurrent
+wanting. To wonder is to occurrently want to know.
+Modulo the exaggeration and other connotations, we might say
+it is to crave or yearn for knowledge.
+
+Besides getting the non-stative-ness right, this
+also works for the merely-some-things-considered and
+the hopeless wondering cases. Craving something but not
+wanting it all-things-considered is very familiar,
+as is yearning for what you know will not happen.
+
+◊section{Why knowledge?}
+
+I've spent all this time talking about the kind of
+wanting involved in wondering, but what it is supposed
+to be a want for?
+Why think it's knowledge?
+To be honest, this is mainly just a hunch. There are various
+alternatives worth considering.
+
+Maybe all
+we want is belief, as Peirce claims.◊note{Peirce (1877)
+"◊link["http://www.peirce.org/writings/p107.html" #:ext 1]{Fixation 
+of Belief}": §IV. Peirce is talking about 'doubt', but
+it seems to me that he has wondering in mind.}
+Or maybe true belief,
+or justified true belief, or stable credence, or rational
+stable credence, or some other non-knowledge-entailing state.
+But I don't think so.
+Peirce observes that one stops wondering
+once one has settled one's opinion into a belief.
+But one also stops yearning for what one mistakenly believes
+one has attained. 
+The desire is not satisfied in such cases,
+but merely dissipates. This is what strikes me as going
+on in cases where one stops wondering but doesn't know.
+
+
+This is not yet a substantive argument against these other
+options,
+but I suspect one can be given (maybe in another post, if
+I can think of an interesting one). 
+
+I'm less confident that the object of a wondering
+desire shouldn't be taken
+to be something something that entails knowledge, but
+involves more. Perhaps wondering Q is wanting to learn Q or to find
+out Q. The usual way one goes from not knowing to knowing is through
+learning or finding out, so it wouldn't be surprising if most
+cases of wanting to know also involve wanting to learn or find out.
+But it does seem possible to come to know without learning or finding
+out.
+Sci-fi examples
+come to mind: one may come to know through some kind of direct
+uploading
+without learning or finding out. I will leave trying
+to figure out if one's
+wondering is satisfied or merely dissipated in such cases to 
+another time. Perhaps in the end we should favor the WTF (Want To
+Find out) over the WTK Theory.
+
+These are still cases
+of coming to know through coming to have a belief. Another way
+one might come to know without learning is going from having a
+belief that is not justified to one that is, or going from 
+Getteried justified true belief to un-Getteried justified
+true belief. It's somewhat unclear
+whether one can really want to undergo such a change, for Moorean
+Paradox-ish reasons, but if one can
+I  suspect we wouldn't want to call it wondering.
+If so, I think the WTK theory can handle this with appeal to some
+form of contrastivism. Something like wanting
+to know Q as opposed to remaining
+in suspended judgement about Q.
+
+
+For now, then, I'm inclined to accept something along the lines
+of
+◊blockquote{◊header{◊b{WTK Theory of Wonder (elaborated)}}
+	◊div{
+S wonders Q iff S occurrently,
+some-things-considered
+wants to know Q rather than
+remain in suspended judgement about Q.
+}}
+
+
+◊section{Metacognition Without Cognitive Concepts}
+
+One more potential counterexample.
+If Samson the puppy◊note{◊(img #:style "max-width:85%" #:src "samson.jpg")}
+		lacks the concept of knowledge
+(plausible), he can't have attitudes towards contents about knowledge.
+So he can't have desires about knowledge. So he can't, e.g.,
+want to know stuff. But he does seem to wonder.
+He wonders what made that sound, which is why he's going to
+the other room.
+But apparently he can't want to know
+what made that sound. So wondering can't be wanting to know.
+
+More generally it seems that we're comfortable describing 
+various non-human animals as wondering, but we wouldn't
+be so comfortable ascribing to them
+a concept of knowledge. Such creatures lack the capacity for
+metacognition, and so must lack the capacity for metacognitive
+desires. So wondering can't be a metacognitive desire like 
+wanting to know.◊note{Friedman (2013, §3) makes this argument,
+as does Carruthers (2018) "◊link["https://doi.org/10.1111/mila.12167" #:ext 1]{Basic Questions}".
+	Whitcomb (2010) "◊link["https://doi.org/10.1111/j.1933-1592.2010.00394.x" #:ext 1]{Curiosity was Framed}" makes the
+	same point about curiosity, building on Kvanvig (2003) 
+	◊i{The Value of Knowledge and the Pursuit of Understanding:} pp. 143ff.}
+
+Attributing to non-human animals mental states with particular
+contents is fraught with philosophical difficulty, so I'm 
+not confident what the best view is here. However,
+I'm unpersuaded that the analysis is refuted.
+
+One reason I'm unpersuaded stems from how I myself feel about the 
+cases. In most contexts, I'm totally happy
+to say that Samson does want to know what made that sound.
+"Why's Samson trying to go there?" "He wants to know what 
+made that sound." This just seems true to me.
+
+Does that mean I think he possesses the concept
+◊small-caps{knowledge}? Well, if all that's meant by
+that is that he can, e.g., want to know what made that sound,
+then sure. But if something more robust is meant, then
+maybe not.
+
+In other contexts---like ones where philosophical
+issues about animals' mental states are raised---I admit
+I am hesitate to say he
+really wants to know what made that sound. But in those
+same contexts I'm similarly hesitant to say he wonders what made that
+sound. Regardless 
+of context, answering the question about why Samson is
+going to the other room with "He wants to know
+what made that sound" seems about equally objectionable
+as
+"He wonders what made that sound".
+This is as the WTK Theory would predict.
+
+The other reason I'm not persuaded is more theoretical.
+I think there are good
+reasons to think that at least some
+kinds of metacognition are less cognitively demanding than
+the skeptics make it out to be, and that
+Samson and other non-human wonderers ◊i{do} 
+represent their own knowledge states in some of their goal
+representations. But that gets into issues
+about psychosemantics 
+best left for their own post. 
+
+In the meantime, email me if you've got other counterexamples to the
+WTK Theory of Wonder. ◊end{}
+
+
